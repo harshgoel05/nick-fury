574b805a3d0c9a983db8c6ed2b0f6ac562f993e5
harshgoel05 <harshgoel05@gmail.com>
2021-06-11 21:11:52 +0530

    ✨ Setup routes

---
+++ src/screens/Login.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Login() {
+  return <div>It's login screen</div>;
+}
---
+++ src/screens/PersonalDetails.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PersonalDetails() {
+  return <div>It's personal details screen</div>;
+}
